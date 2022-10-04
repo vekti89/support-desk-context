f9217b427599fe993296b667e1681fdd823a03c9
vekti89 <you@example.com>
2022-10-04 10:59:50 +0200

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -16,8 +17,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+  //set build folder
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
 app.use(errorHandler);
-app.get('/', (req, res) => {
-  res.send('Server running...');
-});
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
